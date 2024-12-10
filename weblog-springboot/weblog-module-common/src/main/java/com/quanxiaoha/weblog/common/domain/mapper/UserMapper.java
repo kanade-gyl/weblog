4cb5efd6a68a51800360f54dc8215d884929077c
kanade-gyl <3306251266@qq.com>
2024-12-10 13:17:06 +0800

    Add files via upload

--- weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/domain/mapper/UserMapper.java
+++ weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/domain/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.quanxiaoha.weblog.common.domain.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.quanxiaoha.weblog.common.domain.dos.UserDO;
 
 /**
@@ -9,4 +11,9 @@ import com.quanxiaoha.weblog.common.domain.dos.UserDO;
  * @description: TODO
  **/
 public interface UserMapper extends BaseMapper<UserDO> {
+    default UserDO findByUsername(String username) {
+        LambdaQueryWrapper<UserDO> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserDO::getUsername, username);
+        return selectOne(wrapper);
+    }
 }
--- weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/enums/ResponseCodeEnum.java
+++ weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/enums/ResponseCodeEnum.java
@@ -19,6 +19,13 @@ public enum ResponseCodeEnum implements BaseExceptionInterface {
 
     // --------------- 业务异常状态码 ----------------
     PRODUCT_NOT_FOUND("20000","该产品不存在（测试使用）"),
+
+    // --------------- 登录失败响应码 ----------------
+    LOGIN_FAIL("20000", "登录失败"),
+    USERNAME_OR_PWD_ERROR("20001", "用户名或密码错误"),
+
+    UNAUTHORIZED("20002", "无访问权限，请先登录！"),
+
     ;
 
     //异常码
